3deaba99ccc3ca3c5146530134f1a6bead338db2
백재원 <bjw1622@gmail.com>
2023-10-30 16:27:21 +0900

    feat : init

--- 1.basics/ex04/src/App.js
+++ 1.basics/ex04/src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+
 function App() {
-  const App = document.createElement("div");
-  App.textContent = "Hello World";
-  return App;
+  // const App = document.createElement("div");
+  // App.textContent = "Hello World";
+  // const App = React.createElement("div", null, "Hello World");
+  // return App;
+
+  return (
+    <div>
+      Hello Worlddad
+      <ul>
+        {[].map(() => {
+          return <li></li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export { App };
--- 1.basics/ex05/src/App.js
+++ 1.basics/ex05/src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+
 function App() {
-  const App = document.createElement("div");
-  App.textContent = "Hello World";
-  return App;
+  // const App = document.createElement("div");
+  // App.textContent = "Hello World";
+  // const App = React.createElement("div", null, "Hello World");
+  // return App;
+  // return <div>Hello Worlddad</div>;
 }
 
 export { App };

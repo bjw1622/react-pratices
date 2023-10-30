be44229d1e0eeb64bd69ba943ae3997f380277ab
백재원 <bjw1622@gmail.com>
2023-10-30 11:07:57 +0900

    feat : init

--- 1.basics/ex03/public/index.js
+++ 1.basics/ex03/public/index.js
@@ -1,7 +1,3 @@
-function App() {
-  const App = document.createElement("div");
-  App.textContent = "Hello World";
-  return App;
-}
+import { App } from "./App.js";
 
 document.getElementById("root").appendChild(App());

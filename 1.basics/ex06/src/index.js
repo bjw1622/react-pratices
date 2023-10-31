d11d7a2fb0bb4eee0020835cd3e7f2183df514bc
백재원 <bjw1622@gmail.com>
2023-10-31 11:50:02 +0900

    feat : init

---
+++ 1.basics/ex06/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import { App } from "./App.js";
+import React from "react";
+
+const root = ReactDOM.render(App(), document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

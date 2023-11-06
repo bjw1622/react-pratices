85d3f1376a9ab0b0ae6b71626b9efdafccc06120
백재원 <bjw1622@gmail.com>
2023-11-06 16:27:07 +0900

    feat : fetch

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,24 +1,44 @@
-import React, {useState} from 'react';
-import './assets/scss/App.scss';
-import RegisterForm from './RegisterForm';
-import SearchBar from './SearchBar';
-import Emaillist from './Emaillist';
-import data from './assets/json/data';
+import React, { useEffect, useState } from "react";
+import "./assets/scss/App.scss";
+import RegisterForm from "./RegisterForm";
+import SearchBar from "./SearchBar";
+import Emaillist from "./Emaillist";
+// import data from './assets/json/data';
 
 function App() {
-    const [emails, setEmails] = useState(data);
-    const searchEmail = (keyword) => {
-        const newEmails = data.filter(email => email.firstName.indexOf(keyword) !== -1 || email.lastName.indexOf(keyword) !== -1 || email.email.indexOf(keyword) !== -1);
-        setEmails(newEmails);
-    };
-
-    return (
-        <div id={'App'}>
-            <RegisterForm />
-            <SearchBar searchEmail={searchEmail}/>
-            <Emaillist emails={emails} />
-        </div>
+  const [emails, setEmails] = useState(null);
+  const searchEmail = (keyword) => {
+    const newEmails = data.filter(
+      (email) =>
+        email.firstName.indexOf(keyword) !== -1 ||
+        email.lastName.indexOf(keyword) !== -1 ||
+        email.email.indexOf(keyword) !== -1
     );
+    setEmails(newEmails);
+  };
+
+  useEffect(async () => {
+    try {
+      const response = await fetch("/api", {
+        method: "get",
+        header: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  return (
+    <div id={"App"}>
+      <RegisterForm />
+      <SearchBar searchEmail={searchEmail} />
+      {emails === null ? null : <Emaillist emails={emails} />}
+    </div>
+  );
 }
 
-export {App};
+export { App };

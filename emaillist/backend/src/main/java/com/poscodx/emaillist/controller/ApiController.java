349c1d0653db36199eb18cb63543df593de9180a
백재원 <bjw1622@gmail.com>
2023-11-06 15:21:00 +0900

    feat : apicontroller

---
+++ emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.poscodx.emaillist.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poscodx.emaillist.dto.JsonResult;
+
+@RestController
+public class ApiController {
+	@GetMapping("/api")
+	public ResponseEntity<JsonResult> read() {
+		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.success("hello world"));
+	}
+}
